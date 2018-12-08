de754879cd26e60510659dab2c778672c99b18ba
Onno Valkering <onnovalkering@users.noreply.github.com>
2018-12-08 13:32:14 +0700

    Added VS Code extensions list

---
+++ .config/code/extensions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# code --list-extensions | xargs -L 1 echo code --install-extension > extensions.sh 
+
+code --install-extension eg2.tslint
+code --install-extension lfs.vscode-emacs-friendly
+code --install-extension ms-python.python
+code --install-extension octref.vetur
+code --install-extension PeterJausovec.vscode-docker
