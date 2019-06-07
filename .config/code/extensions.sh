bf400f567b6820181e7103d55c73dcb9a9f41f0c
Onno Valkering <onnovalkering@users.noreply.github.com>
2019-06-07 18:31:36 +0200

    Updated VS Code configuration

--- .config/code/extensions.sh
+++ .config/code/extensions.sh
@@ -2,8 +2,12 @@
 
 # code --list-extensions | xargs -L 1 echo code --install-extension > extensions.sh 
 
-code --install-extension eg2.tslint
-code --install-extension lfs.vscode-emacs-friendly
+code --install-extension EditorConfig.EditorConfig
+code --install-extension ms-pyright.pyright
 code --install-extension ms-python.python
-code --install-extension octref.vetur
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension onnovalkering.vscode-singularity
 code --install-extension PeterJausovec.vscode-docker
+code --install-extension redhat.vscode-yaml
+code --install-extension scala-lang.scala
+code --install-extension scalameta.metals
