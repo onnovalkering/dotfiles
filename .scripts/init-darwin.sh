6ab6ccc40b506162ac381cc0b0636ae04bdc46d0
Onno Valkering <onnovalkering@users.noreply.github.com>
2021-06-04 08:46:12 +0200

    Update initialization scripts

--- .scripts/init-darwin.sh
+++ .scripts/init-darwin.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Install Homebrew (provided command).
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ $PWD != $HOME ]]; then
+    echo "The 'init-darwin.sh' script must be run from the home directory."
+    exit 1
+fi
+
+# Install Homebrew.
+if [[ ! exists brew ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if [[ -f Brewfile ]]; then
+    brew bundle
+fi
--- .scripts/init.sh
+++ .scripts/init.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-source ~/.shell_functions
+if [[ $PWD != $HOME ]]; then
+    echo "The 'init.sh' script must be run from the home directory."
+    exit 1
+fi
 
 # Perform OS-specific initialization.
+source ~/.shell_functions
 source ~/.scripts/init-$(lowercase `uname`).sh
 
 # Install system-wide Python packages.
@@ -11,6 +15,6 @@ if [ -f ~/requirements.txt ]; then
     pip3 install -r ~/requirements.txt
 fi
 
-# Remove unnecessary files (locally).
-rm -f ~/LICENSE ~/README.md ~/requirements.txt
+# Remove unnecessary files (locally, not remotely).
+rm -f ~/Brewfile ~/LICENSE ~/README.md ~/requirements.txt
 git ls-files --deleted -z | git update-index --assume-unchanged -z --stdin
