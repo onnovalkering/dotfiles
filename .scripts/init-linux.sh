cbf316ba1905b9ce1fd035a86a5ca2b1cbdc1bc5
Onno Valkering <onnovalkering@users.noreply.github.com>
2021-06-07 19:25:07 +0200

    Add linux install dependency

--- .scripts/init-linux.sh
+++ .scripts/init-linux.sh
@@ -7,8 +7,8 @@ sudo locale-gen en_US.UTF-8
 # Install system dependencies.
 sudo apt-get update && sudo apt install -y \
     gnupg2 \
+    neovim \
     python3-pip
-
 
 # Add CAP_NET_RAW capability to Python (required by automation tool).
 sudo setcap cap_net_raw+ep $(readlink -f $(command -v python3))
