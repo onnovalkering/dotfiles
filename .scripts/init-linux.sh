1771532c117f1be50ce259214ce2b797625940d0
Onno Valkering <onnovalkering@users.noreply.github.com>
2021-06-07 18:20:51 +0200

    Disable confirmation prompts during init.sh

--- .scripts/init-linux.sh
+++ .scripts/init-linux.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 # Install system dependencies.
 sudo apt-get update && sudo apt install -y \
-    python3 \
     python3-pip
 
+# Add CAP_NET_RAW capability to Python (required by automation tool).
+sudo setcap cap_net_raw+ep $(readlink -f $(command -v python3))
